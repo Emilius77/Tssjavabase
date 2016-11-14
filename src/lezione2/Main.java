5fbc09d47692e0f8628f9e7e694113a6681ec88e
tss <tss@debian-tss24>
2016-11-14 11:17:56 +0100

    Salvataggio Lezioni LUCA contenute in un solo file

---
+++ src/lezione2/Main.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lezione2;
+
+
+
+/**
+ * Esempio di commenti in Java
+ * 
+ * @author tss
+ */
+public class Main {
+
+    /**
+     * Da questo metodo "main" partirà l'esecuzione del programma
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        // serve per commentare le singole righe
+        
+        // ------------------------------------------------------
+        
+        /*
+        
+        si usa per comentare più righe
+        senza dover specificare ogni volta, c'è un'apertura ed una chiusura
+        
+        */
+        
+        
+        System.out.println("esempio di commenti ....");
+                
+    } 
+    
+}
