fe70d220baabb020291f536d501a95e73eea5e36
tss <tss@debian-tss24>
2016-12-14 11:05:45 +0100

    Lezione 7 implementazione delle "librerie" personali sul nostro progetto DemoOrdinaArray

---
+++ src/lezione07/DemoOrdinaArray.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lezione07;
+
+import firstLibrary.DateFunction;
+import firstLibrary.LibreriaFunzioniArray;
+import lezione06.ordinamento.*;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author tss
+ */
+public class DemoOrdinaArray {
+
+    public static void main(String[] args) {
+
+        Persona persone[] = new Persona[3];    //creo un array di 3 persone, della classe Persona
+
+        //istanzio l'array di persone
+        persone[0] = new Persona("Mario", "Rossi");
+        persone[1] = new Persona("Gianni", "Verdi");
+        persone[2] = new Persona("Luca", "Bianchi");
+
+        //stampo l'array
+        LibreriaFunzioniArray.stampa(persone);            //scrivendo in questo modo e dando "alt+invio" gli possiamo dire di creare in automatico il metodo "stampa"
+
+        //richiamo il metodo "ordina" passando l'array di persone
+        LibreriaFunzioniArray.ordina(persone);
+
+        //stampo l'array ordinato
+        LibreriaFunzioniArray.stampa(persone);
+        
+        //istanzio l'array Messaggi
+        Messaggio messaggi[] = new Messaggio[3];
+        
+        messaggi[0] = new Messaggio("Arrivederci");
+        messaggi[1] = new Messaggio("Buona sera");
+        messaggi[2] = new Messaggio("Buona notte");
+        
+        //richiamo il metodo "ordina" passando l'array di persone
+        LibreriaFunzioniArray.ordina(messaggi);
+        
+        //stampo l'array ordinato
+        LibreriaFunzioniArray.stampa(messaggi);
+        
+        //cerco un oggetto all'interno dell'array
+        System.out.println(LibreriaFunzioniArray.cerca(persone, new Persona("Luca", "Bianchi")));
+        System.out.println(LibreriaFunzioniArray.cerca(messaggi, new Messaggio("Buona sera")));
+        
+        //richiamo la funzione "Data" e stampo la data corrente nel formato specificato nella libreria
+        System.out.println(DateFunction.convert(new Date()));
+        
+    }
+
+   
+
+    
+}
---
+++ src/lezione07/Persona.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lezione07;
+
+import lezione06.ordinamento.*;
+import java.util.Date;
+import java.util.Objects;
+
+
+/**
+ *
+ * @author tss
+ */
+public class Persona implements Comparable<Persona> {
+
+    private String nome, cognome;
+    private Date dataNascita;
+    private String sesso;
+
+    public Persona() {  //costruttore VUOTO, viene richiesto "sempre" in caso di utlizzo di un DB!
+    }                   //se non si utilizzano DB, si può anche non utilizzare questo tipo di costruttore
+
+    //public Persona(String nome, String cognome) {  //costruttore con soli due campi
+    //    this.nome = nome;
+    //    this.cognome = cognome;
+    //}
+    public Persona(String nome, String cognome) {  //costruttore con soli due campi che richiamo quello più completo!
+        this(nome, cognome, null, null);
+    }
+
+    public Persona(String nome, String cognome, Date dataNascita, String sesso) { //costruttore completo (con tutti i campi richiesti)
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataNascita = dataNascita;
+        this.sesso = sesso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public Date getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(Date dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    public String getSesso() {
+        return sesso;
+    }
+
+    public void setSesso(String sesso) {
+        if (!sesso.equals("F") | !sesso.equals("M")) {
+            return;
+        }
+        this.sesso = sesso;
+    }
+
+    @Override
+    public String toString() {          //l'override viene fatto sempre verso il "padre", in questo caso è il metodo "toString" contenuto in OBJECT
+        return nome + ", " + cognome;
+    }
+    
+    /*
+    @Override
+    public boolean equals(Object obj) {   //in italiano questa sintassi rappresente: se l'oggetto da confrontare è vuoto, restituisci FALSO
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj.getClass() != Persona.class) {  //in italiano questa sintassi rappresente: se l'oggetto da confrontare NON appartiene alla classe PERSONA, restituisci FALSO
+            return false;
+        }
+
+        Persona p = (Persona) obj;      //casting di tipo di variabile
+        if (this.getNome().equals(p.getNome())
+                && this.getCognome().equals(p.getCognome())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    */
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.cognome, other.cognome)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int compareTo(Persona o) {
+        return this.cognome.compareTo(o.getCognome());
+    }
+}
